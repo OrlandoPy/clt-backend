46d38d9daa91dc2fd106e52cf3cd944f448d9caa
Orlando Gomez <ogomez@excelsis.com.py>
2023-01-01 13:46:37 -0300

    Se realizan mejoras en RolUsuarioDTO para mejorar la asignacion de roles

--- src/main/java/com/cltbackend/dto/RolUsuarioDTO.java
+++ src/main/java/com/cltbackend/dto/RolUsuarioDTO.java
@@ -1,9 +1,11 @@
-package com.cltbackend.DTO;
+package com.cltbackend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,6 +13,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class RolUsuarioDTO {
 
-    private String username;
-    private String nombreRol;
+    private Long idUsuario;
+    private List<Long> idRolList;
 }
