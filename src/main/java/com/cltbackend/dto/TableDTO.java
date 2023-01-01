991ac0926478ed3350efe18e9dfec47d523e1265
Orlando Gomez <ogomez@excelsis.com.py>
2023-01-01 13:44:49 -0300

    Creacion de TableDTO para mejorar las respuestas de los listados

---
+++ src/main/java/com/cltbackend/dto/TableDTO.java
@@ -0,0 +1,20 @@
+package com.cltbackend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class TableDTO<T> {
+
+    private List<T> lista;
+
+    private int totalRecords;
+
+   public TableDTO(){
+
+   }
+
+}
