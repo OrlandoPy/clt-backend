905cc8df71c7e4755b3c3ec01d0fc8afbae9becd
Orlando Gomez <ogomez@excelsis.com.py>
2023-01-01 20:19:27 -0300

    Creacion de la clase ServicioDTO

---
+++ src/main/java/com/cltbackend/dto/ServicioDTO.java
@@ -0,0 +1,21 @@
+package com.cltbackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ServicioDTO {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+}
