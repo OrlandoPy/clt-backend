35b56e47a67db8ee0cebb75ebc12215bde3240cc
Orlando Gomez <ogomez@excelsis.com.py>
2023-01-01 17:30:14 -0300

    Optimizacion en los imports de la clase Usuario

--- src/main/java/com/cltbackend/model/Usuario.java
+++ src/main/java/com/cltbackend/model/Usuario.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
