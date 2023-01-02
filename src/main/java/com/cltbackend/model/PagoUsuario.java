f039c953070c4a5be6ad840accf482b7f3c3661b
Orlando Gomez <ogomez@excelsis.com.py>
2023-01-01 23:57:17 -0300

    Creacion del modelo PagoUsuario

---
+++ src/main/java/com/cltbackend/model/PagoUsuario.java
@@ -0,0 +1,45 @@
+package com.cltbackend.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "PAGOS_USUARIOS")
+public class PagoUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "ID_PAGO_USUARIO")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_DEUDA_USUARIO", referencedColumnName = "ID_DEUDA_USUARIO")
+    @NotNull
+    private DeudaUsuario deudaUsuario;
+
+    @Column(name = "MONTO_ABONADO")
+    @NotNull
+    private Long montoAbonado;
+
+    @Column(name = "NRO_REFERENCIA_COMPROBANTE")
+    @NotNull
+    private String nroReferenciaComprobante;
+
+    @Column(name = "FECHA")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull
+    private Date fecha;
+}
