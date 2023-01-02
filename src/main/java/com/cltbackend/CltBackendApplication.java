3d62049e3d867be12b58b98a57595fbb5eef1528
Orlando Gomez <ogomez@excelsis.com.py>
2023-01-02 02:24:31 -0300

    Se eliminar codigo comentado de la clase CltBackendApplication

--- src/main/java/com/cltbackend/CltBackendApplication.java
+++ src/main/java/com/cltbackend/CltBackendApplication.java
@@ -24,27 +24,4 @@ public class CltBackendApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	/*@Bean
-	CommandLineRunner run (UsuarioService usuarioService) {
-		return args -> {
-			usuarioService.saveRol(new Rol(null, "ROL_USER"));
-			usuarioService.saveRol(new Rol(null, "ROL_MANAGER"));
-			usuarioService.saveRol(new Rol(null, "ROL_ADMIN"));
-			usuarioService.saveRol(new Rol(null, "ROL_SUPER_ADMIN"));
-
-			usuarioService.saveUsuario(new Usuario(null, "ogomez@gmail.com", "12345", "Orlando Gomez", "98765432", 0L, new ArrayList<>()));
-			usuarioService.saveUsuario(new Usuario(null, "amendoza@gmail.com", "12345", "Alejandro Mendoza", "2345678", 0L, new ArrayList<>()));
-			usuarioService.saveUsuario(new Usuario(null, "mramirez@gmail.com", "12345", "Marcelo Ramirez", "5624562", 0L, new ArrayList<>()));
-			usuarioService.saveUsuario(new Usuario(null, "nchamorro@gmail.com", "12345", "Nicolas Chamorro", "66775656 ", 0L, new ArrayList<>()));
-
-			usuarioService.addRolToUsuario("ogomez@gmail.com", "ROL_USER");
-			usuarioService.addRolToUsuario("ogomez@gmail.com", "ROL_MANAGER");
-			usuarioService.addRolToUsuario("amendoza@gmail.com", "ROL_MANAGER");
-			usuarioService.addRolToUsuario("mramirez@gmail.com", "ROL_ADMIN");
-			usuarioService.addRolToUsuario("nchamorro@gmail.com", "ROL_SUPER_ADMIN");
-			usuarioService.addRolToUsuario("nchamorro@gmail.com", "ROL_ADMIN");
-			usuarioService.addRolToUsuario("nchamorro@gmail.com", "ROL_USER");
-		};
-	}*/
-
 }
